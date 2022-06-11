0ab168acc78a853fd203fea85ce3aea9ac777685
hatuank97lhp <hatuank97lhp@gmail.com>
2022-06-11 20:41:48 +0700

    fist commit

---
+++ src/error.rs
@@ -0,0 +1,52 @@
+use thiserror::Error;
+
+use cosmwasm_std::StdError;
+
+#[derive(Error, Debug)]
+pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+
+    #[error("Init failed")]
+    InitFailed,
+
+    #[error("DenominationInvalid Invalid")]
+    DenominationInvalid {},
+
+    #[error("Commitment submitted")]
+    CommitmentSubmitted {},
+
+    #[error("Please send `mixDenomination` ORAI along with transaction")]
+    InvalidDenom{},
+    
+    #[error("Fee exceeds transfer value")]
+    FeeExceed{},
+
+    #[error("The note has been already spent")]
+    InvalidRoot{},
+
+    #[error("Cannot find your merkle root")]
+    NullifierUsed{},
+
+    #[error("Invalid withdraw proof")]
+    InvalidProof{},
+
+    #[error("Message value is supposed to be zero for ORAI instance")]
+    InvalidValue{},
+
+    #[error("Refund value is supposed to be zero for ORAI instance")]
+    InvalidRefund{},
+
+    #[error("payment to recipient did not go thru")]
+    FailedToRecipient{},
+
+    #[error("payment to _relayer did not go thru")]
+    FailedToRelayed{},
+
+    #[error("Levels Invalid")]
+    LevelsInvalid {},
+
+    #[error("Merkle Tree Full")]
+    MerkleTreeFull {},
+    
+}
